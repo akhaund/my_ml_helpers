6d6f36bff6c7f1e69f0b1396782e61e078b3a14d
Anshuman Khaund <akhaund@users.noreply.github.com>
2020-10-29 12:41:25 -0400

    Update dimension_reduction.py
    MCA class.

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -175,6 +175,43 @@ class DoMCA:
     def __init__(self) -> None:
         pass
 
+    def do_mca(df,
+               save_plot=None,
+               show_plot: bool = True):
+        """
+        """
+        x = df.values
+        N = np.sum(x)
+        Z = x / N
+
+        sum_r = np.sum(Z, axis=1)
+        sum_c = np.sum(Z, axis=0)
+
+        Z_expected = np.outer(sum_r, sum_c)
+        Z_residual = Z - Z_expected
+
+        D_r_sqrt = np.sqrt(np.diag(sum_r**-1))
+        D_c_sqrt = np.sqrt(np.diag(sum_c**-1))
+
+        mca_mat = D_r_sqrt @ Z_residual @ D_c_sqrt
+        _, S, Qh = svd(mca_mat, full_matrices=False)
+        Q = Qh.T
+
+        G = D_c_sqrt @ Q @ np.diag(S)
+
+        eig_vals = S ** 2
+        expl_var = eig_vals / eig_vals.sum()
+        expl_var = pd.DataFrame({'frac.': expl_var,
+                                 'cumul. frac.': expl_var.cumsum()})
+
+        fig = Plotters.explained_variance_plot(expl_var)
+
+        if show_plot:
+            fig.show()
+        if save_plot is not None:
+            save_plot(fig, 'mca_variance')
+        return G
+
 
 def get_mutual_info(df, labels, discretes,
                     save_plot=None,
@@ -193,48 +230,6 @@ def get_mutual_info(df, labels, discretes,
     if save_plot is not None:
         save_plot(fig, 'mutual_info')
     return mi
-
-
-def do_mca(df,
-           save_plot=None,
-           show_plot: bool = True):
-    """
-    """
-    x = df.values
-    N = np.sum(x)
-    Z = x / N
-
-    sum_r = np.sum(Z, axis=1)
-    sum_c = np.sum(Z, axis=0)
-
-    Z_expected = np.outer(sum_r, sum_c)
-    Z_residual = Z - Z_expected
-
-    D_r_sqrt = np.sqrt(np.diag(sum_r**-1))
-    D_c_sqrt = np.sqrt(np.diag(sum_c**-1))
-
-    mca_mat = D_r_sqrt @ Z_residual @ D_c_sqrt
-    _, S, Qh = svd(mca_mat, full_matrices=False)
-    Q = Qh.T
-
-    G = D_c_sqrt @ Q @ np.diag(S)
-
-    eig_vals = S ** 2
-    explained_variance = eig_vals / eig_vals.sum()
-    explained_variance = \
-        pd.DataFrame({'frac.': explained_variance,
-                      'cumul. frac.': explained_variance.cumsum()}
-                     ).mul(100).round(3)
-    fig = px.line(explained_variance,
-                  y='cumul frac',
-                  labels={'index': 'feature count',
-                          'cumul frac': 'Variance explained (cumul. perc.)',
-                          'title': 'Variance explained by MCA components'})
-    if show_plot:
-        fig.show()
-    if save_plot is not None:
-        save_plot(fig, 'mca_variance')
-    return G
 
 
 # Test
