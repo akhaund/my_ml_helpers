e215c232469b2830d6e6b2b0d8d6586b1598dd9d
Anshuman Khaund <akhaund@users.noreply.github.com>
2020-10-29 21:34:26 -0400

    Update binary_clf_metrics.py
    Minor edits.

--- binary_clf_metrics.py
+++ binary_clf_metrics.py
@@ -9,37 +9,36 @@ import helpers as hp
 
 
 def get_metrics(y_truth, y_pred,
-                positive: int,
-                increment: float):
+                positive: int = 1,
+                increment: float = 0.005):
     """
     """
-    precision = len(str(increment).split('.')[1])  # Has to be a better way
+    precision = len(str(increment).split(".")[1])  # Has to be a better way
     thresholds = [round(i, precision) for i in np.arange(0.0, 1.0, increment)]
-    negative = 1-positive
-    df = pd.DataFrame({'Truth': y_truth,
-                       'Pred_Proba': y_pred})
-
+    negative = 1 - positive
+    df = pd.DataFrame({"Truth": y_truth,
+                       "Pred_Proba": y_pred})
     auc_roc = roc_auc_score(y_truth, y_pred)
     dct_pr, dct_sn, dct_sp = hp.dicts(3, thresholds)
     for t in thresholds:
         # Precision
         try:
-            precision = (df[df['Pred_Proba'] >= t]['Truth']
+            precision = (df[df["Pred_Proba"] >= t]["Truth"]
                          .value_counts(normalize=True)[positive])
         except(KeyError, IndexError):
             precision = None
         # Sensitivity
         try:
-            sensitivity = (df[df['Pred_Proba'] >= t]['Truth']
+            sensitivity = (df[df["Pred_Proba"] >= t]["Truth"]
                            .value_counts()[positive] /
-                           df['Truth'].value_counts()[positive])
+                           df["Truth"].value_counts()[positive])
         except(KeyError, IndexError):
             sensitivity = None
         # Specificity
         try:
-            specificity = (df[df['Pred_Proba'] < t]['Truth']
+            specificity = (df[df["Pred_Proba"] < t]["Truth"]
                            .value_counts()[negative] /
-                           df['Truth'].value_counts()[negative])
+                           df["Truth"].value_counts()[negative])
         except(KeyError, IndexError):
             specificity = None
 
